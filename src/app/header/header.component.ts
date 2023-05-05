e48a94f4e019c7b782f53f7540b7fd8488385e8b
aidul <aidul5613@gmail.com>
2023-05-06 00:14:51 +0600

    search suggestion

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Product } from '../data-type';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-header',
@@ -10,8 +12,9 @@ export class HeaderComponent implements OnInit {
 
   menuType: string = "default";
   sellerName: string = ""; 
+  searchResults: undefined | Product[];
 
-  constructor(private router: Router) {}
+  constructor(private router: Router, private product: ProductService) {}
 
   ngOnInit(): void {
     this.router.events.subscribe((route: any) => {
@@ -29,6 +32,27 @@ export class HeaderComponent implements OnInit {
           }
         }
     });
+  }
+
+  onSearch() {
+    console.log("search button click");
+  }
+
+  searchProduct(query: KeyboardEvent) {
+    if(query) {
+      const elements = query.target as HTMLInputElement;
+      this.product.searchProducts(elements.value).subscribe((res) => {
+        if(res.length>5)
+        {
+          res.length = 5;
+        }
+        this.searchResults = res;
+      });
+    }
+  }
+
+  hideSearch() {
+    this.searchResults = undefined;
   }
 
   logout() {
